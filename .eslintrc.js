7a46e507a99246ac848d7d67a35ce04854fbf82c
Aleksei Kuznetsov <aleksei.kuznetsoff@gmail.com>
2024-03-06 14:06:58 +0200

    chore: add eslint typescript plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,27 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
     'prettier/prettier': 'warn',
   },
   ignorePatterns: ['.eslintrc.js', '*.config.js', '*.config.mjs', 'tailwind.config.ts'],
