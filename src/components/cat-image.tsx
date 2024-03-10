751d38fd1f3d9fa486982f77ae1dfdcfd3e272ac
Aleksei Kuznetsov <aleksei.kuznetsoff@gmail.com>
2024-03-10 13:36:33 +0200

    fix: no random images in prod build

--- src/components/cat-image.tsx
+++ src/components/cat-image.tsx
@@ -1,20 +1,12 @@
 import Image from 'next/image';
 
-import crypto from 'crypto';
-
 export function CatImage() {
-  /**
-   * Random int to provide in URL so that
-   * every time users would get new image
-   */
-  const rand = crypto.randomInt(1000);
-
   return (
     <Image
-      priority
+      unoptimized
       alt="Random photo of a cat"
       height={256}
-      src={`https://cataas.com/cat?width=320&height=256&q=${rand}`}
+      src={`https://cataas.com/cat?width=320&height=256`}
       width={320}
     />
   );
