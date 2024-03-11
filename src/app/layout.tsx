8c42a50da438bf51c91722118b82b5e735d1890a
Aleksei Kuznetsov <aleksei.kuznetsoff@gmail.com>
2024-03-11 16:49:35 +0200

    feat: favicons & others

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html suppressHydrationWarning className={notoSans.variable} lang="en">
+      <head>
+        <link href="/favicon.ico" rel="icon" sizes="32x32" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/manifest.webmanifest" rel="manifest" />
+      </head>
       <body className="dark:bg-zinc-900">
         <Providers>
           <div className="mx-auto w-full max-w-2xl px-6 py-16 max-sm:px-5 max-sm:py-8">
