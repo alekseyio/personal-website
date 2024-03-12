c69b65e7e212b1784f229660db0e32eb616a3af4
Aleksei Kuznetsov <aleksei.kuznetsoff@gmail.com>
2024-03-12 18:47:09 +0200

    feat: custom 404 page

---
+++ src/components/not-found.tsx
@@ -0,0 +1,15 @@
+export function NotFound() {
+  return (
+    <section>
+      <h2>404! Something&apos;s missing...</h2>
+      <p>
+        It seems this page has gone on a little adventure of its own and is currently
+        missing.
+      </p>
+      <p>
+        Please, click the &ldquo;Go back&rdquo; link at the top of your screen to be
+        navigated to the main page.
+      </p>
+    </section>
+  );
+}
